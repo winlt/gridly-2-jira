48e066032b07ede8cb0c12d0c1aea6a62c007121
Win Le <tlv@localizedirect.com>
2021-09-23 10:26:13 +0700

    commit

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-jiraUsername = "tlv@localizedirect.com"
-jiraApiKey = "l36yDj0vrWg11BVZm8Tn3ACB"
-jiraDomain = "https://team-1626146770460.atlassian.net"
-jiraProjectKey = "WT"
+jiraUsername = ""
+jiraApiKey = ""
+jiraDomain = ""
+jiraProjectKey = ""
